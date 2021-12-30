a31ea4197c6cf703ca4ec369cdc449f65c85b7bf
unknown <matt.mcquain@gmail.com>
2021-12-29 21:05:19 -0800

    components boilerplated and navbar started

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+// Allows us to export all of the components from a single file //
+
+export { default as Loader } from './Loader';
+export { default as Navbar } from './Navbar';
+export { default as Services } from './Services';
+export { default as Footer } from './Footer';
+export { default as Transactions } from './Transactions';
+export { default as Welcome } from './Welcome';
