be9b57bc3a044cca707c312065c92d213c81cb65
unknown <matt.mcquain@gmail.com>
2021-12-30 19:25:36 -0800

    card with Ethereum logo displaying correctly. Still working on Welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,74 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./"
+
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
 const Welcome = () => {
+
+  const connectWallet = () => {
+
+  }
+
   return (
-    <h1>
-      Welcome
-    </h1>
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className='flex flex-1 justify-start flex-col md:mr-10'>
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+            Send Crypto <br /> across the world
+          </h1>
+          <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
+            I like turtles. Lots of turtles. Especially turtles that are cute and trade crypto.
+          </p>
+          <button
+            type='button'
+            onClick={connectWallet}
+            className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+          >
+            <p className='text-white text-base font-semibold'>Connect Wallet</p>
+          </button>
+
+          {/* Grid with all our features */}
+          <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>
+              Reliability
+            </div>
+            <div className={commonStyles}>
+              Security
+            </div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>
+              Ethereum
+            </div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>
+              Web 3.0
+            </div>
+            <div className={commonStyles}>
+              Lower Fees
+            </div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>
+              Blockchain
+            </div>
+          </div>
+        </div>
+
+        {/* The right side of our desktop Welcome view */}
+        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+          <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glasmorphism'>
+            <div className='flex justify-between flex-col w-full h-full'>
+              <div className='flex justify-between icons-start'>
+                <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                  <SiEthereum fontSize={21} color='#fff' />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   );
 }
 
