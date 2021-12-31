810b5dd983a2772977db0476892fd35cfca73b05
unknown <matt.mcquain@gmail.com>
2021-12-30 18:20:43 -0800

    Navbar and its toggle menu work. Starting on Welcome component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
@@ -12,6 +13,9 @@ const NavbarItem = ({ title, classProps }) => {
 }
 
 const Navbar = () => {
+  // state shows if navbar is opened or closed
+  const [toggleMenu, setToggleMenu] = useState(false); //or = React.useState(false);
+
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -25,6 +29,29 @@ const Navbar = () => {
           Login
         </li>
       </ul>
+      <div>
+        {/* checks if toggle menu is active. True opens it */}
+        {toggleMenu
+          ? <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
+          : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)} />
+        }
+        {/* Actually shows the menu only when setToggleMenu is set to true */}
+        {/* if we dont use '-right-2' and just use 'right-2', our menu window wont be all the way to the right side of the page */}
+        {toggleMenu && (
+          <ul
+            className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+              flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
+            '
+          >
+            <li className='text-xl w-full my-2'>
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index) => (          
+              <NavbarItem key={item + index} title={item} classProps='my-2 text-lg' />  /* For each iteration we want to show the navbar item */
+            ))}
+          </ul>
+        )}
+      </div>
     </nav>
   );
 }
